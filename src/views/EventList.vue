4ad62652d27e1d24fa8a7644d2ba5f0a1869d581
Juan Camilo Morales <camov1209@gmail.com>
2019-02-24 23:39:39 -0500

    Adds API request for events using axios

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -10,10 +10,22 @@
 
 <script>
 import EventCard from '@/components/EventCard.vue'
+import axios from 'axios'
 
 export default {
   components: {
     EventCard
+  },
+  data() {
+    return {
+      events: []
+    }
+  },
+  created() {
+    axios
+      .get('http://localhost:3000/events')
+      .then(response => (this.events = response.data))
+      .catch(error => console.log('There was an error: ', error.response))
   }
 }
 </script>
